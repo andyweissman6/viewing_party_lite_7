0197c925055022a5068c1a8b852032099b5806d7
Nick Sacco <nicksacco10@gmail.com>
2023-07-04 11:25:32 -0700

    test: party validation tests

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe Party, type: :model do
     it { should have_many(:users).through(:party_users) }
   end
 
-  # describe 'validations' do
-
-  # end
+  describe 'validations' do
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:time) }
+    it { should validate_numericality_of(:duration) }
+  end
 end
