b1a63faa1ad0a665e17c3f29731baa00f5126b7e
Nick Sacco <nicksacco10@gmail.com>
2023-07-04 11:33:31 -0700

    feat: defined user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :party_users
   has_many :parties, through: :party_users
+
+  validates :email, uniqueness: true, presence: true
+  validates  :name, presence: true
 end
