19755f72a1ebbbd807ab4464a50a614610629b79
Nick Sacco <nicksacco10@gmail.com>
2023-07-05 13:22:36 -0700

    test: show spec file created

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "User Dashboard Page '/users/:id'", type: :feature do
+  before(:each) do
+    @user1 = User.create!(name: 'Jim Bob', email: 'Jbob@somewhere.com')
+    @user2 = User.create!(name: 'Tim Timson', email: 'Ttimmer@hotmail.com')
+    @user3 = User.create!(name: 'Google Guy', email: 'Google@gmail.com')
+  end
+end
