33d9be5ad094be7a42eba72bdaead790be894c68
Nick Sacco <nicksacco10@gmail.com>
2023-07-05 15:09:57 -0700

    test: user name at top of dashboard

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe "User Dashboard Page '/users/:id'", type: :feature do
     @user2 = User.create!(name: 'Tim Timson', email: 'Ttimmer@hotmail.com')
     @user3 = User.create!(name: 'Google Guy', email: 'Google@gmail.com')
   end
+
+  describe "When I visit a user's dashboard" do
+    it "Displays the user's name at the top of the page" do
+      visit dashboard_path(@user1)
+      expect(page).to have_content("#{@user1.name}'s Dashboard")
+    end
+  end
 end
