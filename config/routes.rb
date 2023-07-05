1844672293f5f070fc43471462066438ca2695e2
Nick Sacco <nicksacco10@gmail.com>
2023-07-05 16:17:30 -0700

    feat: discover route created with controller and index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/register', to: 'users#new'
-  get "/users/:user_id", to: "users#show", as: :dashboard
+  get '/users/:user_id', to: 'users#show', as: :dashboard
+  get '/users/:user_id/discover', to: 'discover#index', as: :discover
 end
