d10fff82cf485d78af3cf169ce98cc78cb0dc46a
Nick Sacco <nicksacco10@gmail.com>
2023-07-06 16:51:59 -0700

    feat: user movie route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   post '/register', to: 'users#create'
   get '/users/:user_id', to: 'users#show', as: :dashboard
   get '/users/:user_id/discover', to: 'discover#index', as: :discover
+  get '/users/:user_id/movies', to: 'movies#index', as: :user_movies
 end
