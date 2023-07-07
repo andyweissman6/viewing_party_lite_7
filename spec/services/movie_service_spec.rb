388be841a8a7356dab27f6155150d995908c6b81
Andy Weissman <andyweissman6@gmail.com>
2023-07-07 15:09:14 -0600

    unfinished: create tests for MovieService

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -1,8 +1,15 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-
-# RSpec.describe MovieService do
-#   it "can create a connection and consume api" do
-    
-#   end
-# end
+RSpec.describe MovieService do
+  it "can return the top rated movies" do
+    movies = MovieService.new.top_movies
+    movie = movies.first
+require 'pry'; binding.pry
+    expect(movies).to be_a Hash
+    expect(movies[:results]).to be_an Array
+    expect(movie).to have_key(:title)
+    expect(movie[:title]).to be_a String
+    expect(movie).to have_key(:vote_average)
+    expect(movie[:vote_average]).to be_a String
+  end
+end
