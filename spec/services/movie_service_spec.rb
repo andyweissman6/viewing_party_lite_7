eac896f64a7b8451dec492645526b4c051ee8ef8
Nick Sacco <nicksacco10@gmail.com>
2023-07-07 15:38:18 -0700

    fix movie service tests

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe MovieService do
   it "can return the top rated movies" do
-    movies = MovieService.new.top_movies
-    movie = movies.first
-require 'pry'; binding.pry
-    expect(movies).to be_a Hash
-    expect(movies[:results]).to be_an Array
-    expect(movie).to have_key(:title)
-    expect(movie[:title]).to be_a String
-    expect(movie).to have_key(:vote_average)
-    expect(movie[:vote_average]).to be_a String
+    response = MovieService.new.top_movies
+    @movies = response[:results]
+    expect(response).to be_a(Hash)
+    expect(@movies).to be_a(Array)
+    expect(@movies.count).to eq(20)
+    expect(@movies.first).to have_key(:title)
+    expect(@movies.first).to have_key(:vote_average)
+    expect(@movies.first[:title]).to be_a(String)
+    expect(@movies.first[:vote_average]).to be_a(Float)
   end
 end
