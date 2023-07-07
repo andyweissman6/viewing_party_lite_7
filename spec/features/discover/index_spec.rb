44d3ca2fbf64ee75dd69149da087b14f25c25495
Nick Sacco <nicksacco10@gmail.com>
2023-07-06 17:25:40 -0700

    test/feat: added within blocks plus more testing

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -9,7 +9,11 @@ RSpec.describe "Discover Movies Page", type: :feature do
 
   it "Has a button to discover top rated movies" do
     visit discover_path(@user1)
-    expect(page).to have_button('Find Top Rated Movies')
+    within '#discover_movies' do
+      expect(page).to have_button('Find Top Rated Movies')
+      click_button('Find Top Rated Movies')
+    end
+    expect(current_path).to eq(user_movies_path(@user1))
   end
   
   it "Search field to search by movie title" do
